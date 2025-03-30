e72cb12f9b941bf247ea004e401f57e4fdd355e3
Devpod17 <shubhama664@gmail.com>
2025-03-31 00:01:47 +0530

    🎨 Cookie fix

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -2,7 +2,7 @@ export const getCookie = (name: string) => {
     return document.cookie.split('; ').find(row => row.startsWith(`${name}=`))?.split('=')[1];
 }
 export const setCookie = (name: string, value: string) => {
-    document.cookie = `${name}=${value}; path=/`;
+    document.cookie = `${name}=${value}; path=/; max-age=604800`; // expires in 7 days
 }
 export const deleteCookie = (name: string) => {
     document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
